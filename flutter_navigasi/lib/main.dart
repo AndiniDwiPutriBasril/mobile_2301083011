058f4420c8e77c83c9a7bc8244ddab5773274c85
AndiniDwiPutriBasril <167855200+AndiniDwiPutriBasril@users.noreply.github.com>
2025-01-04 12:34:24 +0700

    flutter_navigasi

---
+++ flutter_navigasi/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_navigasi/page_satu.dart';
+
+void main() => runApp(MainApp());
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PageSatu(),
+    );
+  }
+}
